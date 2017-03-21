11417f7fe42ce4d5b3cc0fc4cbdae3d1dfc6e1b5
Peleable <peleable@gmail.com>
2017-03-21 14:51:58 +0700

    Update verify.php

--- verify.php
+++ verify.php
@@ -1,3 +1,5 @@
+$proxy = 'http://fixie:Hr2JXteynC46Il0@velodrome.usefixie.com:80';
+$proxyauth = 'http://fixie:Hr2JXteynC46Il0@velodrome.usefixie.com:80';
 <?php
 $access_token = 'SD1b0qRCgjATTTQlAr4kN5MJyozdhVYSYH0BhoDlkDHtH1bEJLRlApJcPIfUXfUyaa+x3wTi+o0kIzEtY5ZV7pSuKpgCg0ZbSfAXNCt984ej9B/eKo18qDKbVRvLUxav43nupUce4FM6b2Eta1L9rwdB04t89/1O/w1cDnyilFU=';
 
@@ -6,6 +8,8 @@ $url = 'https://api.line.me/v1/oauth/verify';
 $headers = array('Authorization: Bearer ' . $access_token);
 
 $ch = curl_init($url);
+curl_setopt($ch, CURLOPT_PROXY, $proxy);
+curl_setopt($ch, CURLOPT_PROXYUSERPWD, $proxyauth);
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
 curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
 curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
